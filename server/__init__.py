7be97d75eff4d13f589665279d2740025724e395
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-07 23:07:40 -0400

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -1,3 +1,29 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from server.routes import register_routes
+from server.models import db, User
 
-db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__, static_folder='static', template_folder='templates')
+    
+    app.config['SECRET_KEY'] = 'your-secret-key'  # Replace in production
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['UPLOAD_FOLDER'] = 'uploads'
+    app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # 100MB
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    register_routes(app)
+
+    return app
