9b40e0518cad0035aa59419057fa9274ac035dd3
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-12 01:25:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask
-from src.server.models import db
+from flask_login import LoginManager
+from src.server.models import db, User
 from src.server.routes import register_routes
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'  # Replace this in production
+app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///smartdispute.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 with app.app_context():
     db.create_all()
