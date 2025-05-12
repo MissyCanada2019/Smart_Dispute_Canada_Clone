a74d1f51ef154098d481ec0a7f8ae68730dcd231
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-12 02:33:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from src.server.models import db, User
-from src.server.routes import register_routes
-import os
+from flask_mail import Mail
+from config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///smartdispute.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-
-db.init_app(app)
-
+db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.login_view = 'login'
-login_manager.init_app(app)
+mail = Mail()
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-with app.app_context():
-    db.create_all()
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
 
-register_routes(app)
+    from src.server.routes import register_routes
+    register_routes(app)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    return app
