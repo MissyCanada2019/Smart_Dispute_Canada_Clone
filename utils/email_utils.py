70c013885eea2dd53d1f42aba497d473abe329a2
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-13 17:07:18 -0400

    Delete utils/email_utils.py

--- utils/email_utils.py
+++
@@ -1,6 +0,0 @@
-from flask_mail import Message
-from app import mail
-
-def send_email(subject, recipient, body):
-    msg = Message(subject=subject, recipients=[recipient], body=body)
-    mail.send(msg)
