9f61b14e8d43294252a3961a6f0948b65e6cc14d
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-14 12:26:12 -0400

    Update and rename wsgi.py to src/wsgi.py

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,35 +1,30 @@
 import sys
 import os
 
+# Make sure 'server' folder is on the path
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from datetime import datetime
 
-# Add the parent directory of 'server' to the system path
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+from server.models import User
+from server.routes import register_routes
+from server.admin_routes import register_admin_routes
+from server.legal_help import legal_help_bp
 
-# Setup
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 
 app = Flask(__name__)
-
-# Config
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "dev-secret-key")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///site.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Init extensions
 db.init_app(app)
 login_manager.init_app(app)
-
-# Import and register
-from server.models import User
-from server.routes import register_routes
-from server.admin_routes import register_admin_routes
-from server.legal_help import legal_help_bp
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -43,6 +38,5 @@ app.register_blueprint(legal_help_bp)
 def inject_now():
     return {'now': datetime.utcnow()}
 
-# Expose app
-if __name__ != "__main__":
-    application = app
+# Required for Gunicorn
+application = app
