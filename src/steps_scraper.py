23390add3c0a8f0b4204e4cfb971632a5880a3ca
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-14 00:34:22 -0400

    Create steps_scraper.py

---
+++ src/steps_scraper.py
@@ -0,0 +1,13 @@
+# src/steps_scraper.py
+
+import time
+
+def run_scraper():
+    print("Steps to Justice scraper running...")
+    # TODO: Add real scraping logic here
+    # This is just a placeholder for now
+    time.sleep(5)
+    print("Scraping complete.")
+
+if __name__ == "__main__":
+    run_scraper()
