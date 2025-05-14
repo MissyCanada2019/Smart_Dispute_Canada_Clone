884402954f13aa876a17954c88e86cf5432d123d
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-14 10:45:14 -0400

    Update and rename app.py to src/app.py

--- src/app.py
+++ src/app.py
@@ -6,22 +6,27 @@ import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+login_manager.login_view = 'login'
 
 def create_app():
     app = Flask(__name__)
+
+    # Config
     app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "dev-secret-key")
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///site.db")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
 
-    from server.models import User
+    # Import User model from models.py
+    from models import User
+
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
+    # Import and register routes
     from server.routes import register_routes
     from server.admin_routes import register_admin_routes
     from server.legal_help import legal_help_bp
