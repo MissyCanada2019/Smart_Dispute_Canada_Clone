924acadd970e838fcab9cd5d10b35d65263850af
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-15 01:08:55 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,42 +1,10 @@
 import sys
 import os
 
-# Add 'src' to the system path so we can import from the 'server' package
+# Add 'src' to the system path so we can import app factory
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from datetime import datetime
+from server import create_app
 
-from models import User
-from server.routes import register_routes
-from server.admin_routes import register_admin_routes
-from server.legal_help import legal_help_bp
-
-db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.login_view = 'login'
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "dev-secret-key")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///site.db")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
-login_manager.init_app(app)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-app = register_routes(app)
-app = register_admin_routes(app)
-app.register_blueprint(legal_help_bp)
-
-@app.context_processor
-def inject_now():
-    return {'now': datetime.utcnow()}
-
-# Required for Gunicorn
-application = app
+# This will be picked up by Gunicorn as `wsgi:application`
+application = create_app()
