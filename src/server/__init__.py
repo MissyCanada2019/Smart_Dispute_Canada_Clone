64650fac7ab2c039fb37cd1274f3a9304e765bd7
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-16 19:00:06 -0400

    Update __init__.py

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -3,10 +3,11 @@ from flask import Flask
 from datetime import datetime
 
 from src.server.extensions import db, login_manager
+# REMOVE: from src.models import User
 
 def create_app():
     app = Flask(__name__)
-    
+
     # Config
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "dev-secret")
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///site.db")
@@ -18,6 +19,9 @@ def create_app():
     login_manager.init_app(app)
     login_manager.login_view = "login"
 
+    # DELAY import of models UNTIL AFTER init_app
+    from src.models import User
+
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
@@ -26,21 +30,18 @@ def create_app():
     from src.server.routes import register_routes
     app = register_routes(app)
 
-    # Optional admin routes
     try:
         from src.server.admin_routes import register_admin_routes
         app = register_admin_routes(app)
     except ImportError:
         pass
 
-    # Optional blueprint for legal help
     try:
         from src.server.legal_help import legal_help_bp
         app.register_blueprint(legal_help_bp)
     except ImportError:
         pass
 
-    # Context for Jinja2
     @app.context_processor
     def inject_now():
         return {'now': datetime.utcnow()}
