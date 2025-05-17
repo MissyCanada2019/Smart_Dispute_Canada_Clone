a7def9292a48f56f1d8fb4ce2f46ac9b6aef2e65
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-17 00:18:43 -0400

    Update __init__.py

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask  # This is the missing import!
+from flask import Flask
 from datetime import datetime
+
 from src.server.extensions import db, login_manager
 
 def create_app():
     app = Flask(__name__)
 
-    # Config
+    # Configuration
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "dev-secret")
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///site.db")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -17,17 +18,18 @@ def create_app():
     login_manager.init_app(app)
     login_manager.login_view = "login"
 
-    # Import AFTER init to avoid circular import
+    # Import models AFTER initializing db
     from src.models import User
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    # Register routes
+    # Register main routes
     from src.server.routes import register_routes
     app = register_routes(app)
 
+    # Register optional routes
     try:
         from src.server.admin_routes import register_admin_routes
         app = register_admin_routes(app)
@@ -40,11 +42,9 @@ def create_app():
     except ImportError:
         pass
 
-        @app.context_processor
+    # Global template variables
+    @app.context_processor
     def inject_now():
         return {'now': datetime.utcnow()}
-
-    with app.app_context():
-        db.create_all()
 
     return app
