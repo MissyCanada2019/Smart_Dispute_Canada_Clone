84141c8afcca9ee979e8c4b3918b53202dcef057
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-17 16:37:51 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,15 @@
 import sys
 import os
 
-# Add 'src' to system path so we can import the server package
+# Add 'src' to system path
 sys.path.append(os.path.abspath('src'))
 
 from server import create_app
 from server.extensions import db
 
-# Create the Flask app using your application factory
-app = create_app()
+application = create_app()
+app = application
 
-# Create missing PostgreSQL tables like "user"
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
