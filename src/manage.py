69ee892ca00b7cace3f6fc993e8992888871c3ac
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-18 19:24:11 -0400

    Update manage.py

--- src/manage.py
+++ src/manage.py
@@ -1,12 +1,13 @@
+# manage.py
+
 from flask.cli import FlaskGroup
 from src.server import create_app
 from src.server.extensions import db
 from flask_migrate import Migrate
 
+# Create app and CLI group
 app = create_app()
 cli = FlaskGroup(app)
-
-# Setup Flask-Migrate
 migrate = Migrate(app, db)
 
 if __name__ == "__main__":
