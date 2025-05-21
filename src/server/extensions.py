fc0f9eaa72e757ed2063e44c0d04a9e654944de6
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-21 01:18:17 -0400

    Update extensions.py

--- src/server/extensions.py
+++ src/server/extensions.py
@@ -2,7 +2,9 @@ import os
 import requests
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_wtf import CSRFProtect
 
+csrf = CSRFProtect()
 # Initialize extensions
 db = SQLAlchemy()
 login_manager = LoginManager()
