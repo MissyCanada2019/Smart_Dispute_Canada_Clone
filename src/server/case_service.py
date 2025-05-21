c7012878553f324a389ff4fa94fdb46f4e21aaa8
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-20 22:42:49 -0400

    Create case_service.py

---
+++ src/server/case_service.py
@@ -0,0 +1,30 @@
+from flask import current_app
+from src.server.extensions import db
+from src.server.models import Case
+
+def handle_upload(form_data, user_id):
+    new_case = Case(
+        user_id=user_id,
+        case_type=form_data.get("case_type"),
+        province=form_data.get("province"),
+        facts=form_data.get("facts"),
+        respondent=form_data.get("respondent"),
+        violations=form_data.get("violations"),
+    )
+    db.session.add(new_case)
+    db.session.commit()
+    return new_case
+
+def prepare_review_data(case_id):
+    case = Case.query.get(case_id)
+    if not case:
+        return None
+
+    return {
+        "case_id": case.id,
+        "case_type": case.case_type,
+        "province": case.province,
+        "facts": case.facts,
+        "respondent": case.respondent,
+        "violations": case.violations,
+    }
