f230c72e08c8e9cb786ed957cf7298303dad37ce
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-23 15:40:50 -0400

    Update __init__.py

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -1,20 +1,5 @@
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect
-
-from src.server.extensions import db, login_manager
-from src.routes.main_routes import main as main_bp
-from src.routes.auth_routes import auth_bp
-from src.routes.admin_cases import admin_bp
-from src.server.doc_routes import doc_bp  # This should be here if used
-
-csrf = CSRFProtect()
-
 def create_app():
-src/server/../../templates => src/templates/
+    app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your-secret-key')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///../instance/app.db')
@@ -25,7 +10,6 @@ src/server/../../templates => src/templates/
     Migrate(app, db)
     csrf.init_app(app)
 
-    # Blueprints must be registered inside the function
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(admin_bp)
