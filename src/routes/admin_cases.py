6071544d9d91ce22fc9f775191992578521f44be
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-23 09:35:38 -0400

    Update admin_cases.py

--- src/routes/admin_cases.py
+++ src/routes/admin_cases.py
@@ -1,4 +1,4 @@
-<from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 from src.models import User, Case
 from src.server.extensions import db
