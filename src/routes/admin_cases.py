00a962172d26b25d9d403a66077219aae8a54e16
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-23 09:28:03 -0400

    Update admin_cases.py

--- src/routes/admin_cases.py
+++ src/routes/admin_cases.py
@@ -1,16 +1,19 @@
-<from flask import Blueprint, render_template, redirect, url_for, flash
+<# src/routes/admin_cases.py
+
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 from src.models import User, Case
+from src.server.extensions import db
 
-admin_bp = Blueprint("admin", __name__, url_prefix="/admin")
+admin_bp = Blueprint("admin", __name__)
 
-@admin_bp.route("/dashboard")
+@admin_bp.route("/admin")
 @login_required
 def admin_dashboard():
     if not current_user.is_admin:
         flash("Access denied.", "danger")
         return redirect(url_for("main.dashboard"))
 
-    users = User.query.all()
     cases = Case.query.all()
-    return render_template("admin_dashboard.html", users=users, cases=cases)
+    users = User.query.all()
+    return render_template("admin_dashboard.html", cases=cases, users=users)
