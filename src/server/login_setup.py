e3df22b8ba19834f69879ab1454afc6d8b36cc59
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-23 23:13:53 -0400

    Update login_setup.py

--- src/server/login_setup.py
+++ src/server/login_setup.py
@@ -1,5 +1,9 @@
-# src/server/login_setup.py
 from src.models import User
+from flask_login import LoginManager
 
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
