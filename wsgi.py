b80b00fc4b69c5e681dd28f0d7375376ebe1c264
MissyCanada2019 <terri.bertin57@gmail.com>
2025-05-24 01:12:43 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-# Ensure src is in path
-sys.path.append(os.path.abspath("src"))
+# Add the absolute path to the src directory
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
 
 from server import create_app
 
 application = create_app()
-app = application  # optional alias for local dev
+app = application  # For local dev or Flask CLI
